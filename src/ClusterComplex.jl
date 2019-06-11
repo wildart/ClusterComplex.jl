fb449ee5ec15109a341c068b837940ccbcd0984b
Art Wild <wildart@gmail.com>
2019-06-11 17:24:32 -0400

    major overhaul, added MC object, plots & datasets

--- src/ClusterComplex.jl
+++ src/ClusterComplex.jl
@@ -4,13 +4,39 @@ import ComputationalHomology: complex, group, simplices, persistenthomology, dim
 import Clustering: ClusteringResult, nclusters, assignments
 import Distances
 import MultivariateStats: fit, PCA, mean, projection
-import Statistics: cov, mean
-import LinearAlgebra: inv, pinv, norm
+import Statistics: mean, covm
+import LinearAlgebra: inv, pinv, norm, eigen, isposdef, diagm, diag
 
 export clustercomplex
 
+include("types.jl")
 include("datasets.jl")
 include("dominance.jl")
+include("plotrecipe.jl")
+
+function invcov(S)
+    return try
+        inv(S)
+    catch
+        pinv(S) # use pseudoinverse
+    end
+end
+
+function rescalecov!(C)
+    F = eigen(C)
+    V = F.values
+
+    # replace negative eigenvalues by zero
+    V = max.(V, 0)
+
+    # reconstruct correlation matrix
+    CC = F.vectors * diagm(0=>V) * F.vectors'
+
+    # rescale correlation matrix
+    T = 1 ./ sqrt.(diag(CC))
+    C = CC .* (T * T')
+    return C
+end
 
 """Calculate the distances from points, `X`, to a linear manifold with `basis` vectors, translated to `origin` point.
 
@@ -43,113 +69,60 @@ function distance_to_manifold(X::AbstractMatrix{T}, origin::AbstractVector{T}, b
 end
 
 """
-    pointToManifoldMahalanobis(X, μ, B, assignments)
+    mahalonobis(X, partition)
 
-Calculate Mahalanobis distance in a point to linear manifold distance space which is constructed from the data `X`.
+Calculate a Mahalonobis distance from `partition` of the dataset `X`.
 
 # Arguments
 - `X::AbstractMatrix`: the dataset matrix
-- `μ::AbstractVector`: the partition mean
-- `B::AbstractMatrix`: the partition basis
-- `assignments`: the partition point assignments
+- `partition::Vector{Int}`: the partition indexes
 """
-function pointToManifoldMahalanobis(X::AbstractMatrix{T}, μ::AbstractVector{T}, B::AbstractMatrix{T},
-                                    assignments::AbstractVector{<:Integer}) where T <: Real
-    # calculate distances for all data points to
+function mahalonobis(data::AbstractMatrix{<:Real}, partition::Vector{Int})
+    pdata = view(data, :, partition)
+    # calculate Mahalonobis distance parameters
+    μ = vec(mean(pdata, dims=2))
+    C = covm(pdata, μ, 2)
+    if !isposdef(C)
+        error("Covariance matrix is not positive definite")
+        # rescalecov!(C)
+    end
+    dist = Distances.Mahalanobis(invcov(C))
+    D = Distances.colwise(dist, data, μ)
+    return μ, C, D
+end
+
+"""
+    ssmahalonobis(X, partition[; kwargs...])
+
+Calculate Mahalanobis distance in a point to linear manifold distance
+space which is constructed from from `partition` of the dataset `X`.
+
+# Arguments
+- `X::AbstractMatrix`: the dataset matrix.
+- `partition::Vector{Int}`: the partition of the dataset.
+- `subspacemaxdim::Integer = size(data,1)-1`: the maximal dimension of the partition subspace
+"""
+function ssmahalonobis(data::AbstractMatrix{T}, partition::Vector{Int};
+                       subspacemaxdim::Integer = size(data,1)-1) where {T<:Real}
+    # calculate projection to PC subspace
+    m = fit(PCA, data[:,partition], maxoutdim=subspacemaxdim)
+
     # a linear manifold and its orthoganal compliment
-    D1 = distance_to_manifold(X, μ, B)
-    D2 = distance_to_manifold(X, μ, B; ocss=true)
+    D1 = distance_to_manifold(data, mean(m), projection(m))
+    D2 = distance_to_manifold(data, mean(m), projection(m); ocss=true)
 
     # use distances as space dimensions
-    δ = hcat(D1, D2)'
+    δ = hcat(D1, D2)
 
     # determine covariance matrix from the distances associated with cluster
-    δs = δ[:,assignments]
-    S = cov(δs, dims=2)
-    S⁻¹ = try
-        inv(S)
-    catch
-        pinv(S) # use pseudoinverse
-    end
+    δs = δ[partition, :]
+    μ = mean(δs, dims=1)
+    C = covm(δs, μ, 1)
 
     # calculate Mahalanobis to all the points with cluster-derived covariance matrix
-    return Distances.colwise(Distances.Mahalanobis(S⁻¹), δ, zeros(T, size(δ)...)), δ, S
-end
-
-function invcov(data)
-    S = cov(data, dims=2)
-    S⁻¹ = try
-        inv(S)
-    catch
-        pinv(S) # use pseudoinverse
-    end
-    return S⁻¹
-end
-
-"""
-    mahalonobis!(D, data, partition::ClusteringResult, χ[; kwargs...])
-
-Fill the distance matrix `D` using a Mahalonobis distance from `partition` to `data` points.
-
-# Arguments
-- `data::AbstractMatrix`: the dataset matrix.
-- `partition::ClusteringResult`: the partition of the dataset.
-- `χ::Real`: the maximal radius of the sphere.
-- `subspacemaxdim::Integer = size(data,1)-1`: the maximal dimension of the partition subspace
-- `maxoutdim::Integer`: the maximal dimension of the simplicial complex
-- `ν::Integer=0`: the witness comples construction mode (see `witness`)
-"""
-function mahalonobis!(D::AbstractMatrix{T},
-                      data::AbstractMatrix{T},
-                      partition::P) where {T <: Real, P <: ClusteringResult}
-
-    N = size(data,2)         # number of points
-    K = nclusters(partition) # number of partitions
-
-    size(D) != (N,K) && throw(DimensionMismatch("Incorrect distance matrix size: $(size(D)) != ($N,$K)"))
-
-    # calculate distance matrix from partitions
-    assign = assignments(partition)
-    for i in 1:K
-        pdataidxs = assign .== i
-        pdata = view(data, :, pdataidxs)
-        dist = Distances.Mahalanobis(invcov(pdata))
-        μ = vec(mean(pdata, dims=2))
-        D[:, i] .= Distances.colwise(dist, data, μ)
-    end
-end
-
-"""
-    subspacemahalonobis(data, partition::ClusteringResult, χ[; kwargs...])
-
-Construct a simplicial complex from `data` using its `partition` and outputs its filtration up to a maximal radius `χ`.
-
-# Arguments
-- `data::AbstractMatrix`: the dataset matrix.
-- `partition::ClusteringResult`: the partition of the dataset.
-- `χ::Real`: the maximal radius of the sphere.
-- `subspacemaxdim::Integer = size(data,1)-1`: the maximal dimension of the partition subspace
-- `maxoutdim::Integer`: the maximal dimension of the simplicial complex
-- `ν::Integer=0`: the witness comples construction mode (see `witness`)
-"""
-function subspacemahalonobis!(D::AbstractMatrix{T},
-                              data::AbstractMatrix{T},
-                              partition::P;
-                              subspacemaxdim::Integer = size(data,1)-1) where {T <: Real, P <: ClusteringResult}
-
-    N = size(data,2)         # number of points
-    K = nclusters(partition) # number of partitions
-
-    size(D) != (N,K) && throw(DimensionMismatch("Incorrect distance matrix size: $(size(D)) != ($N,$K)"))
-
-    # calculate distance matrix from partitions
-    assign = assignments(partition)
-    for i in 1:K
-        pdataidxs = assign .== i
-        pdata = view(data, :, pdataidxs)
-        m = fit(PCA, pdata, maxoutdim=subspacemaxdim)
-        D[:, i] .= first(pointToManifoldMahalanobis(data, mean(m), projection(m), pdataidxs))
-    end
+    dist = Distances.Mahalanobis(invcov(C))
+    D = Distances.colwise(dist, data, zeros(T, size(μ,2)))
+    return mean(m), C, D
 end
 
 """
@@ -165,32 +138,48 @@ Returns a simplicial complex with weights evaluated from distances.
 - `method::Symbol = :mahalonobis`: the default construction method
 - `maxoutdim::Integer = 1`: the maximal dimension of the simplicial complex
 - `expansion::Symbol = :incremental`: the default simplicial complex construction method
-- `ν::Integer=2`: the witness comples construction mode (see `witness`)
+- `ν::Integer=0`: the witness comples construction mode (see `witness`)
 - `subspacemaxdim::Integer = size(data,1)-1`: the maximal dimension of the partition subspace
 """
 function clustercomplex(data::AbstractMatrix{T}, partition::P, χ::T;
                         method=:mahalonobis,
                         maxoutdim::Integer = 1,
                         expansion = :incremental,
-                        ν::Integer=2,
+                        ν::Integer=0,
                         kwargs...) where {T <: Real, P <: ClusteringResult}
 
-    N = size(data,2)          # number of points
+    M, N = size(data)         # number of points
     K = nclusters(partition)  # number of partitions
     D = Array{T}(undef, N, K) # distance matrix from partitions
+    mc = Array{MahalonobisCluster}(undef, K)
+
+    # get additional parameters
+    params = filter(p->first(p) == :subspacemaxdim, kwargs)
+    ssmaxdim = get(params, :subspacemaxdim, size(data,1)-1)
 
     # calculate distance matrix with appropriate method
-    if method == :mahalonobis
-        mahalonobis!(D, data, partition, kwargs...)
-    elseif method == :subspacemahalonobis
-        params = filter(p->first(p) == :subspacemaxdim, kwargs)
-        ssmaxdim = get(params, :subspacemaxdim, size(data,1)-1)
-        subspacemahalonobis!(D, data, partition, subspacemaxdim=ssmaxdim)
-    else
-        throw(ArgumentError("Invalid method name $(method)"))
+    assign = assignments(partition)
+    for i in unique(assign)
+        pdataidxs = assign .== i
+        idxs = (1:N)[pdataidxs]
+        μ, C, dist = if method == :mahalonobis
+            mahalonobis(data, idxs)
+        elseif method == :subspacemahalonobis
+            ssmahalonobis(data, idxs, subspacemaxdim=ssmaxdim)
+        else
+            throw(ArgumentError("Invalid method name $(method)"))
+        end
+        # println("i=",i)
+        # println("μ=",μ)
+        # println("C=",C)
+        # println("size:",size(pdata))
+        # println("pd:", isposdef(C))
+        mc[i] = MahalonobisCluster(μ, C, idxs)
+        D[:, i] .= dist
     end
 
-    return witness(D', χ, ν=ν, maxoutdim=min(maxoutdim, K), expansion=expansion)
+    cplx, w = witness(D', χ, ν=ν, maxoutdim=min(maxoutdim, K), expansion=expansion)
+    return cplx, w, MahalonobisClusteringResult(mc)
 end
 
 end
---
+++ src/types.jl
@@ -0,0 +1,19 @@
+import Clustering: ClusteringResult, nclusters, assignments, counts
+
+struct MahalonobisCluster{T<:Real}
+    mu::Vector{T}
+    sigma::Matrix{T}
+    idx::Vector{Int}
+end
+
+struct MahalonobisClusteringResult <: ClusteringResult
+    clusters::Vector{MahalonobisCluster}
+    MahalonobisClusteringResult(mcs::Vector{MahalonobisCluster}) = new(mcs)
+end
+Base.show(io::IO, result::MahalonobisClusteringResult) =
+    print(io, "MahalonobisClustering: $(length(result.clusters)) clusters")
+
+nclusters(mcr::MahalonobisClusteringResult) = length(mcr.clusters)
+counts(mcr::MahalonobisClusteringResult) = map(c->length(c.idx), mcr.clusters)
+assignments(mcr::MahalonobisClusteringResult) =
+    map(first, sort!([(i,j) for (i,c) in enumerate(mcr.clusters) for j in c.idx], by=e->e[2]))

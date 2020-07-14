bf7a43ae40a5a91cbc6aacd18da94dfecd445f0c
Art Wild <wildart@gmail.com>
2020-07-14 18:56:03 -0400

    added new custom clustering constructor

--- src/types.jl
+++ src/types.jl
@@ -13,7 +13,9 @@ models(R::ModelClusteringResult) = R.models
 """User-defined clustering"""
 struct CustomClusteringResult <: ClusteringResult
     assignments::Vector{Int}
+    centers::Matrix
 end
+CustomClusteringResult(a::Vector{Int}) = CustomClusteringResult(a, zeros(0,0))
 Base.show(io::IO, R::CustomClusteringResult) = print(io, "Custom Clustering: $(nclusters(R)) clusters")
 counts(R::CustomClusteringResult) = [count(==(i), R.assignments) for i in unique(R.assignments)]
 nclusters(R::CustomClusteringResult) = length(counts(R))
